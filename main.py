fd76b37645b16b6a22f4142869268c7f3f4117ee
Ashley Zhang <ayz27@cornell.edu>
2025-04-15 18:44:19 -0400

    add dir creation

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import os
+
+ALL_DATA_PATH = "data/"
+RAW_DATA_PATH = "raw/"
+CONVERTED_DATA_PATH = "png/"
+
+def make_dirs():
+    if not os.path.exists(ALL_DATA_PATH):
+        os.makedirs(ALL_DATA_PATH)
+
+    if not os.path.exists(ALL_DATA_PATH + RAW_DATA_PATH):
+        os.makedirs(ALL_DATA_PATH + RAW_DATA_PATH)
+    if not os.path.exists(ALL_DATA_PATH + CONVERTED_DATA_PATH):
+        os.makedirs(ALL_DATA_PATH + CONVERTED_DATA_PATH)
+
 if __name__ == "__main__":
+    make_dirs()
     print("Hello world!")
