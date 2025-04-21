b5ae7a3be8408458c14e034a2ce211ae0a663aed
Ashley Zhang <ayz27@cornell.edu>
2025-04-21 16:36:47 -0400

    add converstion to heic

---
+++ file_conversion.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+from pillow_heif import register_heif_opener
+register_heif_opener()
+
+from PIL import Image
+
+def convert_heic_to_png(input_dir, output_dir=None):
+    """
+    Convert all HEIC images in input_dir to PNG format.
+    If output_dir is not specified, images will be saved in the same directory.
+    """
+    if output_dir is None:
+        output_dir = input_dir
+    else:
+        os.makedirs(output_dir, exist_ok=True)
+    
+    heic_files = [f for f in os.listdir(input_dir) if f.lower().endswith('.heic')]
+
+    for heic_file in heic_files:
+        heic_path = Path(os.path.join(input_dir, heic_file))
+        png_path = Path(os.path.join(output_dir, Path(heic_file).stem + '.png'))
+
+        if not png_path.exists():
+            image = Image.open(heic_path)
+            image.save(png_path)
+        
+        assert(png_path.exists() and png_path.is_file())
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
+from file_conversion import convert_heic_to_png
 
-ALL_DATA_PATH = "data/"
-RAW_DATA_PATH = "raw/"
-CONVERTED_DATA_PATH = "png/"
+ROOT_DATA_PATH = "data/"
+RAW_DATA_PATH = ROOT_DATA_PATH + "raw/"
+CONVERTED_DATA_PATH = ROOT_DATA_PATH + "png/"
 
 def make_dirs():
-    if not os.path.exists(ALL_DATA_PATH):
-        os.makedirs(ALL_DATA_PATH)
+    if not os.path.exists(ROOT_DATA_PATH):
+        os.makedirs(ROOT_DATA_PATH)
 
-    if not os.path.exists(ALL_DATA_PATH + RAW_DATA_PATH):
-        os.makedirs(ALL_DATA_PATH + RAW_DATA_PATH)
-    if not os.path.exists(ALL_DATA_PATH + CONVERTED_DATA_PATH):
-        os.makedirs(ALL_DATA_PATH + CONVERTED_DATA_PATH)
+    if not os.path.exists(RAW_DATA_PATH):
+        os.makedirs(RAW_DATA_PATH)
+    if not os.path.exists(CONVERTED_DATA_PATH):
+        os.makedirs(CONVERTED_DATA_PATH)
 
 if __name__ == "__main__":
     make_dirs()
-    print("Hello world!")
+    convert_heic_to_png(RAW_DATA_PATH, CONVERTED_DATA_PATH)
